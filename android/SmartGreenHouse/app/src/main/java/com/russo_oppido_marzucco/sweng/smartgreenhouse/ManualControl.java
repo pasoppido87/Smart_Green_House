5783d8d93755157936b89c1f47a3f80b68159e47
Pasquale Oppido <Pasquale Oppido>
2016-07-14 09:42:49 +0200

    App Android. E' stata implementata la funzionalità di controllo manuale da remoto degli attuatori.

--- android/SmartGreenHouse/app/src/main/java/com/russo_oppido_marzucco/sweng/smartgreenhouse/ManualControl.java
+++ android/SmartGreenHouse/app/src/main/java/com/russo_oppido_marzucco/sweng/smartgreenhouse/ManualControl.java
@@ -54,13 +54,213 @@ public boolean manual=false;
         super.onCreate(savedInstanceState);
         Intent newint = getIntent();
         address = newint.getStringExtra(Dati.EXTRA_ADDRESS);
-        isBtConnected = Dati.isBtConnected;
-        btSocket = Dati.btSocket;
+        isBtConnected=Dati.isBtConnected;
+        btSocket=Dati.btSocket;
 
         setContentView(R.layout.activity_manual_control);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+            new ConnectBT().execute();
+
+        Switch manualSwitch = (Switch)findViewById(R.id.manualSwitch);
+        final Switch luceSwitch = (Switch)findViewById(R.id.luceSwitch);
+        final Switch ventolaSwitch=(Switch)findViewById(R.id.VentolaSwitch);
+        final Switch irrigazioneSwitch = (Switch) findViewById(R.id.irrigazioneSwitch);
+        final Switch riscaldamentoSwitch = (Switch)findViewById(R.id.riscaldamentoSwitch);
+
+        manualSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    sendData(1);
+                    setManualDisplay(isChecked);
+                }
+                else{
+                   luceSwitch.setChecked(false);
+                    ventolaSwitch.setChecked(false);
+                    irrigazioneSwitch.setChecked(false);
+                    riscaldamentoSwitch.setChecked(false);
+                    setManualDisplay(isChecked);
+                    sendData(0);
+                }
+
+            }
+        });
+
+
+        luceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    sendData(3);
+                    ImageView imageView = (ImageView)findViewById(R.id.luceImg);
+                    imageView.setImageResource(R.mipmap.luce_on);
+                }
+                else if(!isChecked){
+
+                    sendData(2);
+                    ImageView imageView = (ImageView)findViewById(R.id.luceImg);
+                    imageView.setImageResource(R.mipmap.luce_off);
+                }
+            }
+        });
+
+
+       ventolaSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                   sendData(5);
+                    ImageView imageView = (ImageView)findViewById(R.id.ventolaImg);
+                    imageView.setImageResource(R.mipmap.ventola_on);
+                }
+                else if(!isChecked){
+                    sendData(4);
+                    ImageView imageView = (ImageView)findViewById(R.id.ventolaImg);
+                    imageView.setImageResource(R.mipmap.ventola_off);
+                }
+            }
+        });
+
+        irrigazioneSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    sendData(7);
+                    ImageView imageView = (ImageView)findViewById(R.id.irrigazioneImg);
+                    imageView.setImageResource(R.mipmap.irrigazione_on);
+
+                }
+                else if(!isChecked){
+                    sendData(6);
+                    ImageView imageView = (ImageView)findViewById(R.id.irrigazioneImg);
+                    imageView.setImageResource(R.mipmap.irrigazione_off);
+
+                }
+            }
+        });
+
+        riscaldamentoSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    sendData(9);
+                    ImageView imageView = (ImageView)findViewById(R.id.riscaldamentoImg);
+                    imageView.setImageResource(R.mipmap.riscaldamento_on);
+
+                }
+                else if(!isChecked){
+                    sendData(8);
+                    ImageView imageView = (ImageView)findViewById(R.id.riscaldamentoImg);
+                    imageView.setImageResource(R.mipmap.riscaldamento_off);
+
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sendData(0);
+    }
+
+    public void setManualDisplay(boolean isManual ){
+        LinearLayout luceLayout=(LinearLayout) findViewById(R.id.luceLayout);
+        LinearLayout ventilaLayout=(LinearLayout) findViewById(R.id.ventolaLayout);
+        LinearLayout irrigazioneLayout=(LinearLayout) findViewById(R.id.irrigazioneLayout);
+        LinearLayout riscaldamentoLayout=(LinearLayout) findViewById(R.id.riscaldamentoLayout);
+        if(isManual){
+            luceLayout.setVisibility(View.VISIBLE);
+            ventilaLayout.setVisibility(View.VISIBLE);
+            irrigazioneLayout.setVisibility(View.VISIBLE);
+            riscaldamentoLayout.setVisibility(View.VISIBLE);
+            this.manual=true;
+        }else{
+            luceLayout.setVisibility(View.GONE);
+            ventilaLayout.setVisibility(View.GONE);
+            irrigazioneLayout.setVisibility(View.GONE);
+            riscaldamentoLayout.setVisibility(View.GONE);
+            this.manual=false;
+        }
+    }
+
+
+
+
+    private void sendData(int i1)
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write(Integer.toString(i1).getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Errore!");
+            }
+        }
+    }
+
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>
+    {
+        private boolean ConnectSuccess = true;
+
+        @Override
+        protected void onPreExecute()
+        {
+                progress = ProgressDialog.show(ManualControl.this, "In connessione...", "Attendere prego!!!");
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices)
+        {
+            try
+            {
+                if ((btSocket == null || !isBtConnected))
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+                msg("Connessione fallita! Prova di nuovo.");
+                finish();
+            }
+            else
+            {
+                msg("Connesso!");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+    }
+
 
 }
